ba327fa2290d81a5fedecf0d49bece1ee09d9c56
alexwdd <alex_wdd@sina.com.cn>
2019-10-11 00:28:22 +0800

    1011

--- application/www/controller/Login.php
+++ application/www/controller/Login.php
@@ -4,14 +4,21 @@ namespace app\www\controller;
 class Login extends Common
 {
 	public function index()
-	{
+	{ 
 		return view();
 	}
 
-	public function register(){   
-        $config = tpCache('sms');
-        $this->assign('config',$config);
-        return view();
+    public function qrcode(){
+        $config = tpCache('weixin');
+        //$notify = urlencode('http://'.$_SERVER['HTTP_HOST'].'/api/wechat/login');
+        $notify = 'http://www.aumaria.com/api/wechat/login';
+        $url = 'https://open.weixin.qq.com/connect/qrconnect?appid='.$config['WEB_APPID'].'&redirect_uri='.$notify.'&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect';
+        $errorCorrectionLevel = 'Q';//纠错级别：L、M、Q、H
+        $matrixPointSize = 10;//二维码点的大小：1到10
+
+        require_once EXTEND_PATH.'qrcode/qrcode.php';
+        $object = new \QRcode();
+        $object->png($url, false , $errorCorrectionLevel, $matrixPointSize, 2);//
     }
 
     function signout(){
